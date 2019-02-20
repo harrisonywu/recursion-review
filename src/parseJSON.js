d25a6b9940d0f8b292033f29a8e7d00e404d4476
harrisonywu <harrisonywu@gmail.com>
2019-02-19 20:31:35 -0800

    Made value function and laid out format for recursive descent parser

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,7 +1,96 @@
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
 
+//boolean, 
+//string, number, array, object, null, undefined, function
+
+// object
+//     {}
+//     { members }
+// members
+//     pair
+//     pair , members
+// pair
+//     string : value
+// array
+//     []
+//     [ elements ]
+// elements
+//     value 
+//     value , elements
+// value
+//     string -> done
+//     number -> done
+//     object -> done
+//     array  -> done
+//     true -> done 
+//     false -> done 
+//     null  -> done
+//      undefined??
+
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var objParser = function(obj) {
+    //two cases: no members, and members
+    // if no members -> return '{}'
+    // if members -> go to the members function
+    var mainObj = {};
+    if (obj === '{}') {
+      return mainObj;
+    } else {
+      var firstMember = membersParser(/* first member*/);
+      return;
+    }
+  };
+  var membersParser = function(members) {
+    //two cases: only one pair, or multiple pairs
+    // if only one pair, then
+  }; 
+  var pairParser = function (pair) {
+    var pairObj = {};
+    
+    return; 
+  };
+  
+  var arrayParser = function(array) {
+    var arr = [];
+    if (array === '[]') {
+      return arr;
+    } else {
+      arr.push();
+      array.substring();
+      return arrayParser(array);
+    }
+    return elementsParser(); //this should return an array
+  };
+  
+  var elementsParser = function(elements) {
+    if (elements.length === 0) {
+      return 0;
+    }
+    var element = elements[0];
+    return valueParser(element);
+  };    
+  
+  var valueParser = function(value) {
+    if (value.charAt(1) === '"' && value.charAt(value.length - 2) === '"') {
+      return value.substring(1, value.length - 1);
+    } else if (typeof Number(value) === 'number') {
+      return Number(value);
+    } else if (value.substring(1, value.length - 1) === 'true') {
+      return true;
+    } else if (value.substring(1, value.length - 1) === 'false') {
+      return false;
+    } else if (value.substring(1, value.length - 1) === 'null') {
+      return null;
+    } else if (value.substring(1, value.length - 1) === 'undefined') {
+      return undefined;
+    } else if (value.charAt(1) === '[' && value.charAt(value.length - 2) === ']') {
+      return arrayParser(value);
+    } else if (value.charAt(1) === '{' && value.charAt(value.length - 2) === '}') {
+      return objParser(value);
+    }
+  };
+
+  
 };
